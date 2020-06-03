2eed4702b659d0e3aa82c0796b765909f189b7c5
Bala Kuppusamy <bkuppusa@uncc.edu>
2020-06-02 20:18:47 -0400

    added props; changed to yml

--- src/main/java/com/hello/api/HelloController.java
+++ src/main/java/com/hello/api/HelloController.java
@@ -6,14 +6,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hello.api.model.Response;
+import com.hello.config.props.HelloProps;
 
 @RestController
 @RequestMapping(value = "/hello")
 public class HelloController {
 
+  private final HelloProps helloProps;
+
+  public HelloController(HelloProps helloProps) {
+    this.helloProps = helloProps;
+  }
+
   @Hidden
   @GetMapping(value = "/hello")
   public Response hello() {
-    return new Response(0, "Hello World!!!");
+    String msg = this.helloProps.getMessage() + " : " + this.helloProps.getFromId();
+    return new Response(0, msg);
   }
 }
---
+++ src/main/java/com/hello/config/PropsConfig.java
@@ -0,0 +1,9 @@
+package com.hello.config;
+
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationPropertiesScan("com.hello.config.props")
+public class PropsConfig {
+}
